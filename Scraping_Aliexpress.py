d64574c89d1d7bf9049fb1f3d7d19f624db11d43
Rubcoh <rubcoh@gmail.com>
2020-11-08 23:09:02 +0200

    Modified README

--- Scraping_Aliexpress.py
+++ Scraping_Aliexpress.py
@@ -1,6 +1,6 @@
 """
 This program is a web scraper for the website Aliexpress.com.
-It allows to retrieve data on prices, titles, shipping, quantity sold, ratings and sellers of smartphones.
+It allows to retrieve data on prices, titles, shipping, quantity sold, ratings and sellers or smartphones.
 """
 
 # Here are the packages that we use in our program
@@ -13,6 +13,7 @@ from selenium.webdriver.common import keys
 from selenium.webdriver.chrome.options import Options
 import time
 import requests
+import random
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 # Here is a list of proxies on which we switch regularly
@@ -106,7 +107,7 @@ def get_data(my_path, my_list, nb_pages):
             for col in range(1, 6):
                 try:
                     # Here we scrape the data following a grid of items on the page (nb_rows X nb_columns) and append each element to a list
-                    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/ul/div[' + str(row) + ']/li[' + str(col) + my_path)))
+                    element = WebDriverWait(driver, 80).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/ul/div[' + str(row) + ']/li[' + str(col) + my_path)))
                     driver.execute_script("arguments[0].scrollIntoView(true);", element)
                     time.sleep(1)
                     my_list.append(element.text)
@@ -125,7 +126,7 @@ def get_data(my_path, my_list, nb_pages):
 
 
 #Here we run the get_data() function on each kind of items we want to scrape from the website
-NB_PAGES = 2
+NB_PAGES = 1
 
 get_data(TITLES_PATH, titles, NB_PAGES)
 get_data(DELIVERY_PATH, delivery, NB_PAGES)
