19c33eeda1a6b359539ce260d8545e8b54eac768
Rubcoh <rubcoh@gmail.com>
2020-11-08 21:35:05 +0200

    Better management of Nan's and Dataframes

--- Scraping_Aliexpress.py
+++ Scraping_Aliexpress.py
@@ -1,3 +1,9 @@
+"""
+This program is a web scraper for the website Aliexpress.com.
+It allows to retrieve data on prices, titles, shipping, quantity sold, ratings and sellers or smartphones.
+"""
+
+# Here are the packages that we use in our program
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,20 +16,22 @@ import time
 import requests
 from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
 import random
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-proxies = ['157.230.247.57:3128', '157.230.247.57:3128', '157.230.247.57:3128', '157.230.247.57:3128', '157.230.247.57:3128', '157.230.247.57:3128']
-
-PROXY = '157.230.247.57:3128'
-webdriver.DesiredCapabilities.CHROME['proxy'] = {
-    "httpProxy": PROXY,
-    "ftpProxy": PROXY,
-    "sslProxy": PROXY,
-
-    "proxyType": "MANUAL",
-}
+# Here is a list of proxies on which we switch regularly
+proxies = ['157.230.247.57:3128', '157.230.247.57:3128', '157.230.247.57:3128',
+           '157.230.247.57:3128', '157.230.247.57:3128', '157.230.247.57:3128']
 
 def switch_proxy(proxies):
+    """
+    This function switches randomly from one proxy to another, given a list of proxies.
+    :param proxies:
+    :return: new random proxy
+    """
+
+    # Generates a random index
     number = random.randint(0, len(proxies) - 1)
+    # Randomly selects a proxy in the proxy list
     PROXY = proxies[number]
     webdriver.DesiredCapabilities.CHROME['proxy'] = {
         "httpProxy": PROXY,
@@ -31,22 +39,27 @@ def switch_proxy(proxies):
         "sslProxy": PROXY,
         "proxyType": "MANUAL",
     }
-    print(F"Proxy switched to {PROXY}")
+    print(f"Proxy switched to {PROXY}")
 
 
+# Here we set our browser preferences and options
 options = Options()
+# Here we setup a user-agent
 options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7")
+# Here we decide whether if we run the program headless or not
 options.headless = False
+# Here we disable notifications
 options.add_argument("--disable-notifications")
-
+# Here we disable pictures display
 prefs = {"profile.managed_default_content_settings.images": 2}
 options.add_experimental_option("prefs", prefs)
 
 
+# Here we setup the webdriver with all the previous options
 DRIVER_PATH = "/Users/ruben/Desktop/Softwares/chromedriver"
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 
-
+# Here we initialize a list for each type of data we want to scrape on the website
 titles = ['Titles']
 delivery = ['Delivery']
 prices = ['Prices']
@@ -54,6 +67,7 @@ qty_sold = ['Quantity sold']
 ratings = ['Ratings']
 stores = ['Stores']
 
+# Here are the variable part of each xpath on which we will loop to obtain each item of the website
 titles_path = ']/div/div[2]/div/div[1]/a'
 delivery_path = ']/div/div[2]/div/div[3]/span'
 prices_path = ']/div/div[2]/div/div[2]/div[1]/span'
@@ -62,53 +76,78 @@ ratings_path = ']/div/div[2]/div/div[7]/a/span'
 stores_path =']/div/div[2]/div/div[8]/a'
 
 
-def get_data(my_path, my_list):
+def get_data(my_path, my_list, nb_pages):
+    """
+    Given a variable part of xpath, a list and a number of pages, this function allows to fill the list with data
+    scraped from the Aliexpress.com smartphones pages
+    :param my_path:
+    :param my_list:
+    :param nb_pages:
+    :return: None
+    """
+    # Here we switch proxy
     switch_proxy(proxies)
+    # Here is the path of the page on which we want to scrape data
     WEBSITE_PATH = "https://fr.aliexpress.com/category/205006047/cellphones.html"
+    # Here we maximize the window to display and scrape more content
     driver.maximize_window()
+    # Here we go to the home page
     driver.get(WEBSITE_PATH)
-    for pages in range(1, 2):
+
+    for pages in range(1, nb_pages + 1):
+        time.sleep(2)
         try:
-            WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[7]/div[2]/div/a"))).click()
-        except Exception:
+            # Here we close the popup appearing at each opening or refresh of a page
+            close_popup = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[7]/div[2]/div/a")))
+            close_popup.click()
+        except TimeoutException:
             pass
+
         for row in range(1, 13):
             for col in range(1, 6):
                 try:
-                    element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/ul/div[' + str(row) + ']/li[' + str(col) + my_path)))
+                    # Here we scrape the data following a grid of items on the page (nb_rows X nb_columns)
+                    element = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/ul/div[' + str(row) + ']/li[' + str(col) + my_path)))
                     driver.execute_script("arguments[0].scrollIntoView(true);", element)
+                    time.sleep(2)
                     my_list.append(element.text)
                     print(f"Found element {row}X{col} of page {pages} while scraping {my_list[0]}")
                 except Exception:
                     print(f"Couldn't find element {row}X{col} of page {pages} while scraping {my_list[0]}")
-                    break
-        WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div/button[2]'))).click()
+                    my_list.append(None)
+        time.sleep(2)
+        try:
+            # Here go to the next page
+            page_number = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div/div/button[' + str(pages) + ']')))
+            page_number.click()
+        except Exception:
+            print("Couldn't reach the page button")
+            break
 
 
+#Here we run the get_data() function on each kind of items we want to scrape from the website
+NB_PAGES = 5
 
-
-get_data(titles_path, titles)
+get_data(titles_path, titles, NB_PAGES)
 time.sleep(random.randint(1, 2))
-get_data(delivery_path, delivery)
-#time.sleep(random.randint(1, 2))
-#get_data(prices_path, prices)
-#time.sleep(random.randint(1, 2))
-#get_data(qty_sold_path, qty_sold)
-#time.sleep(random.randint(1, 2))
-#get_data(ratings_path, ratings)
-#time.sleep(random.randint(1, 2))
-#get_data(stores_path, stores)
+get_data(delivery_path, delivery, NB_PAGES)
+time.sleep(random.randint(1, 2))
+get_data(prices_path, prices, NB_PAGES)
+time.sleep(random.randint(1, 2))
+get_data(qty_sold_path, qty_sold, NB_PAGES)
+time.sleep(random.randint(1, 2))
+get_data(ratings_path, ratings, NB_PAGES)
+time.sleep(random.randint(1, 2))
+get_data(stores_path, stores, NB_PAGES)
 
-
-
-print(titles)
-print(delivery)
-#print(prices)
-#print(qty_sold)
-#print(ratings)
-#print(stores)
-print(len(titles))
-print(len(delivery))
-
-df = pd.DataFrame(titles, delivery)
+# Finally, we create a pandas dataframe with the lists we created and we store it into a csv file
+df = pd.DataFrame(
+    {'titles': titles,
+     'delivery': delivery,
+     'prices': prices,
+    'qty_sold': qty_sold,
+    'ratings': ratings,
+    'stores': stores,
+    })
+df.to_csv("/Users/ruben/Desktop/df.csv")
 print(df)
